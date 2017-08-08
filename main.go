e051fe4280bcc18481be678711f4622c0a694e5c
tj-jianbing <lixiaofeng5609@163.com>
2017-08-08 16:58:51 +0800

    test interface
    test interface

--- main.go
+++ main.go
@@ -2,6 +2,30 @@ package main
 
 import "fmt"
 
+type conncetion interface {
+	Name() string
+	Open()
+	Exec()
+}
+
+type Phone struct {
+	name string
+}
+
+func (con Phone) Name() string {
+	return con.name
+}
+
+func (con Phone) Open() {
+	fmt.Println("test open interface")
+}
+
+func (con Phone) Exec() {}
+
 func main() {
+	var v1 conncetion
+	v1 = Phone{"T1"}
+	v1.Open()
+
 	fmt.Println("hello world!")
 }
